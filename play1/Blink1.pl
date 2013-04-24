20cdd8a3fd9d03e5857d6734b76e5f309acecc80
Bill Waggoner <ctgreybeard@gmail.com>
2013-04-24 14:40:07 -0400

    Testing GPIO modules using Blink1.pl

---
+++ play1/Blink1.pl
@@ -0,0 +1,85 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use 5.14.0;
+use Carp;
+
+use HiPi::Device::GPIO qw/ :pinstatus /;
+use HiPi::Constant qw/:raspberry /;
+
+my ( $dev, $pin, $status, $my_pin, $pin_mode );
+
+sub name_status($) {
+    my $status = shift;
+    my $name   = "Undetermined";
+
+    $name = "DEV_GPIO_PIN_STATUS_NONE" if $status == DEV_GPIO_PIN_STATUS_NONE;
+    $name = "DEV_GPIO_PIN_STATUS_EXPORTED"
+      if $status == DEV_GPIO_PIN_STATUS_EXPORTED;
+  return $name;
+} ## end sub name_status($)
+
+sub name_mode($) {
+    my $status = shift;
+    my $name   = "Undetermined";
+
+    $name = "RPI_PINMODE_INPT" if $status == RPI_PINMODE_INPT;
+    $name = "RPI_PINMODE_OUTP" if $status == RPI_PINMODE_OUTP;
+    $name = "RPI_PINMODE_ALT0" if $status == RPI_PINMODE_ALT0;
+    $name = "RPI_PINMODE_ALT1" if $status == RPI_PINMODE_ALT1;
+    $name = "RPI_PINMODE_ALT2" if $status == RPI_PINMODE_ALT2;
+    $name = "RPI_PINMODE_ALT3" if $status == RPI_PINMODE_ALT3;
+    $name = "RPI_PINMODE_ALT4" if $status == RPI_PINMODE_ALT4;
+    $name = "RPI_PINMODE_ALT5" if $status == RPI_PINMODE_ALT5;
+  return $name;
+} ## end sub name_mode($)
+
+$dev = HiPi::Device::GPIO->new() or croak "Cannot create device";
+
+$my_pin = RPI_PAD1_PIN_18;
+
+say "Using GPIO pin: ", $my_pin;
+$status = $dev->pin_status( $my_pin );
+
+say "Current status is: ", name_status( $status );
+
+say "Exporting...";
+$dev->export_pin( $my_pin );
+
+$status = $dev->pin_status( $my_pin );
+
+say "Current status is: ", name_status( $status );
+
+$pin = $dev->get_pin( $my_pin ) or croak "Cannot create pin";
+say "Got the pin ...";
+
+$pin_mode = $pin->mode();
+say "Current mode is: ", name_mode( $pin_mode );
+
+say "Setting to OUTP";
+$pin->mode( RPI_PINMODE_OUTP );
+
+$pin_mode = $pin->mode();
+say "Current mode is: ", name_mode( $pin_mode );
+
+say "Current active_low is: ", $pin->active_low();
+say "Setting active_low: off";
+$pin->active_low( 0 );
+
+for ( 1 .. 4 ) {
+    say "Reading value...";
+    my $pin_val = $pin->value();
+    my $new_val = $pin_val == RPI_HIGH ? RPI_LOW : RPI_HIGH;
+    say "Current value=", $pin_val, ", setting to ", $new_val;
+    $pin->value( $new_val );
+    sleep( 2 );
+} ## end for ( 1 .. 4 )
+
+say "Unexporting";
+$dev->unexport_pin( $my_pin );
+
+$status = $dev->pin_status( $my_pin );
+
+say "Current status is: ", name_status( $status );
